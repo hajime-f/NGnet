0f9f92302f9a7c77d0e4c7c10990c59d70896821
Hajime Fujita <hajime.fujita@gmail.com>
2021-06-30 20:55:55 +0900

    Sigma no

--- ngnet.py
+++ ngnet.py
@@ -29,10 +29,10 @@ class NGnet:
         
         # The constructor initializes mu, Sigma and W.
         for i in range(M):
-            self.mu.append(np.array([0.0] * N))
+            self.mu.append(2 * np.random.rand(N, 1) - 1)
         
         for i in range(M):
-            x = np.array([1.0] * N)
+            x = [random.random() for i in range(N)]
             self.Sigma.append(np.diag(x))
 
         for i in range(M):
@@ -48,9 +48,6 @@ class NGnet:
         for i in range(M):
             v = np.array([1.0] * D)
             self.var.append(np.diag(v))
-        
-        # for i in range(M):
-        #     self.posterior_i.append([])
         
 
     ### The functions written below are to calculate the output y given the input x
@@ -76,9 +73,9 @@ class NGnet:
 
         sum_g_j = 0
         for j in range(self.M):
-            sum_g_j += multivariate_normal.pdf(x, mean=self.mu[j], cov=self.Sigma[j])
+            sum_g_j += multivariate_normal.pdf(x, mean=self.mu[j].flatten(), cov=self.Sigma[j])
         
-        g_i = multivariate_normal.pdf(x, mean=self.mu[i], cov=self.Sigma[i])
+        g_i = multivariate_normal.pdf(x, mean=self.mu[i].flatten(), cov=self.Sigma[i])
 
         N_i = g_i / sum_g_j
         
@@ -96,24 +93,17 @@ class NGnet:
     
     ### The functions written below are to learn the parameters according to the EM algorithm.
 
-    # This function calculates equation (2.2)
-    def calc_P_xyi(self, x, y, i):
-
-        # Equation (2.3a)
-        P_i = 1 / self.M
-
-        # Equation (2.3b)
-        P_x = multivariate_normal.pdf(x, mean=self.mu[i], cov=self.Sigma[i])
-
-        # Equation (2.3c)
-        diff = y.reshape(-1, 1) - self.linear_regression(x, i)
-        P_y = multivariate_normal.pdf(x, mean=diff.flatten(), cov=self.var[i])
-
-        # Equation (2.2)
-        P_xyi = P_i * P_x * P_y
+    def offline_learning(self, x_list, y_list):
         
-        return P_xyi
-    
+        if len(x_list) != len(y_list):
+            print('Error: The number of input vectors x is not equal to the number of output vectors y.')
+            exit()
+        else:
+            self.T = len(x_list)
+
+        self.offline_E_step(x_list, y_list)
+        self.offline_M_step(x_list, y_list)
+
 
     # This function executes E-step written by equation (3.1)
     def offline_E_step(self, x_list, y_list):
@@ -129,26 +119,56 @@ class NGnet:
             self.posterior_i.append(p_t)
 
             
+    # This function calculates equation (2.2)
+    def calc_P_xyi(self, x, y, i):
+
+        # Equation (2.3a)
+        P_i = 1 / self.M
+
+        # Equation (2.3b)
+        P_x = multivariate_normal.pdf(x, mean=self.mu[i].flatten(), cov=self.Sigma[i])
+
+        # Equation (2.3c)
+        diff = y.reshape(-1, 1) - self.linear_regression(x, i)
+        P_y = multivariate_normal.pdf(x, mean=diff.flatten(), cov=self.var[i])
+
+        # Equation (2.2)
+        P_xyi = P_i * P_x * P_y
+        
+        return P_xyi
+    
+            
     # This function executes M-step written by equation (3.4)
     def offline_M_step(self, x_list, y_list):
         
+        self.offline_mu_update(x_list)
+        self.offline_Sigma_update(x_list)
         
-        
-        pass
+    
+
+    # This function updates mu accrding to equation (3.4a)
+    def offline_mu_update(self, x_list):
+
+        for i, mu_i in enumerate(self.mu):
+            sum_1 = 0
+            sum_mu = 0
+            for t, x_t in enumerate(x_list):
+                sum_1 += self.posterior_i[t][i]
+                sum_mu += x_t.T * self.posterior_i[t][i]
+            self.mu[i] = sum_mu / sum_1
 
 
-    def offline_learning(self, x_list, y_list):
-        
-        if len(x_list) != len(y_list):
-            print('Error: The number of input vectors x does not correspend to the number of output vectors y.')
-            exit()
-        else:
-            self.T = len(x_list)
+    # This function updates Sigma according to equation (3.4b)
+    def offline_Sigma_update(self, x_list):
 
-        self.offline_E_step(x_list, y_list)
-        self.offline_M_step(x_list, y_list)
-        
-            
+        for i, Sigma_i in enumerate(self.Sigma):
+            sum_1 = 0
+            sum_diff = 0
+            for t, x_t in enumerate(x_list):
+                sum_1 += self.posterior_i[t][i]
+                diff = x_t - self.mu[i].T
+                sum_diff += (diff * diff.T) * self.posterior_i[t][i]
+            self.Sigma[i] = sum_diff / sum_1
             
         
 
@@ -160,8 +180,19 @@ if __name__ == '__main__':
     
     ngnet = NGnet(N, D, M)
 
-    # x = 2 * np.random.rand(N, 1) - 1
-    x = np.array([0.4, 0.5, 0.3, 0.2]).reshape(-1, 1)
+    x_list = []
+    y_list = []
+    T = 100
+    
+    for t in range(T):
+        x_list.append(2 * np.random.rand(N, 1) - 1)
+        y_list.append(2 * np.random.rand(D, 1) - 1)
 
-    y = ngnet.get_output_y(x)
+    for x_t in x_list:
+        print(ngnet.get_output_y(x_t))
+        
+    ngnet.offline_learning(x_list, y_list)
+
+
+    # x = np.array([0.4, 0.5, 0.3, 0.2]).reshape(-1, 1)
     
